eeb45b23294d82cd560ac54c8ca6523c51245335
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-09-23 16:38:28 -0500

    Install heroicons package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge'],
+  build: {
+    transpile: ['@heroicons/vue']
+  }
 })
