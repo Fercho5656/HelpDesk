14441625a7eb174cfc47c84b896175e867feab20
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-09-23 16:39:07 -0500

    Add input component for passwords

---
+++ components/ui/PasswordInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <label class="relative mt-3 block text-sm font-medium text-gray-900 dark:text-gray-300">
+    <p class="absolute top-0 -translate-y-full">{{label}}</p>
+    <button class="absolute top-1/2 w-7 h-7 -translate-y-1/2 left-3" @click="showPassword = !showPassword">
+      <eye-slash-icon v-if="showPassword" />
+      <eye-icon v-else />
+    </button>
+    <input
+      class="appearance-none block w-full py-3 px-4 pl-14 rounded-md bg-gray-200 dark:bg-gray-500 dark:placeholder-gray-300 focus:outline-none"
+      :type="showPassword ? 'text' : 'password'" :placeholder="placeholder" :value="modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)">
+  </label>
+</template>
+
+<script setup lang="ts">
+
+import { EyeIcon, EyeSlashIcon } from '@heroicons/vue/24/solid/'
+import { ComputedRef } from 'nuxt/dist/app/compat/vue-demi';
+
+interface Props {
+  label: string
+  placeholder?: string
+  modelValue: string | number
+}
+
+const props = defineProps<Props>()
+
+const showPassword = ref<boolean>(false)
+</script>
+
+<style scoped>
+
+</style>
