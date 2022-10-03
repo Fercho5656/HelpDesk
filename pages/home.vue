d5dee3b1485cbdfce36ff2dfc1d6cbedc25bfa9d
Fercho5656 <morenomoranfernando@gmail.com>
2022-10-03 01:15:29 -0500

    Add home page whilst uses protect routes middleware

---
+++ pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Bienvenido!! haz iniciado sesión</h1>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['protect-routes']
+})
+
+const user = useSupabaseUser()
+
+onMounted(() => {
+  watchEffect(() => {
+    if (!user.value) {
+      navigateTo('/')
+    }
+    console.log(user.value)
+  })
+})
+</script>
+
+<style scoped>
+
+</style>
