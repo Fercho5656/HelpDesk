b5029f6d113d24f3059b045dbb805a034f0ac5eb
Fercho5656 <morenomoranfernando@gmail.com>
2022-10-03 16:46:48 -0500

    Move login, register and home page into the same page

--- components/Register.vue
+++ components/Register.vue
@@ -1,48 +1,32 @@
 <template>
-  <div class="bg-gray-50 dark:bg-slate-800 h-screen grid grid-cols-12">
-    <div class="flex flex-col col-span-12 md:col-span-7">
-      <nuxt-link class="p-3" to="/" @click="darkMode = !darkMode">
-        <icons-campo-azul />
-      </nuxt-link>
-      <form @submit.prevent="submigRegister"
-        class="w-full h-full flex flex-col self-center justify-center gap-y-12 md:w-9/12 p-5">
-        <h2 class="dark:text-gray-50 mb-3 text-2xl font-bold text-center">Crear Cuenta</h2>
-        <div class="flex flex-col gap-y-10">
-          <u-i-input type="email" label="Correo Electrónico" placeholder="name@pfatequila.com" v-model="email" />
-          <u-i-password-input label="Contraseña" v-model="password" />
-        </div>
-        <button class="w-full py-3 dark:bg-cyan-900 bg-slate-800 text-gray-50 rounded-xl">Crear Cuenta</button>
-        <p class="flex justify-center dark:text-gray-50">¿Ya tienes una cuenta?&nbsp; <nuxt-link to="/"
-            class="text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300">Inicia Sesión
-          </nuxt-link>
-        </p>
-      </form>
-    </div>
-    <div class="hidden md:block col-span-5">
-      <nuxt-img class="h-full object-cover" src="/auth-bg.jpeg" placeholder alt="" />
-    </div>
+  <div class="flex flex-col col-span-12 md:col-span-7">
+    <nuxt-link class="p-3" to="/">
+      <icons-campo-azul />
+    </nuxt-link>
+    <form @submit.prevent="submigRegister"
+      class="w-full h-full flex flex-col self-center justify-center gap-y-12 md:w-9/12 p-5">
+      <h2 class="dark:text-gray-50 mb-3 text-2xl font-bold text-center">Crear Cuenta</h2>
+      <div class="flex flex-col gap-y-10">
+        <u-i-input type="email" label="Correo Electrónico" placeholder="name@pfatequila.com" v-model="email" />
+        <u-i-password-input label="Contraseña" v-model="password" />
+      </div>
+      <button class="w-full py-3 dark:bg-cyan-900 bg-slate-800 text-gray-50 rounded-xl">Crear Cuenta</button>
+      <p class="flex justify-center dark:text-gray-50">¿Ya tienes una cuenta?&nbsp; <nuxt-link to="#"
+          @click="$emit('show-login')"
+          class="text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300">Inicia Sesión
+        </nuxt-link>
+      </p>
+    </form>
   </div>
-
 </template>
 
 <script setup lang="ts">
-import { login, signUp } from '../services/auth'
+import { signUp } from '../services/auth'
 
-definePageMeta({
-  middleware: ['protect-routes']
-})
+defineEmits<{
+  (e: 'show-login'): void
+}>()
 
-const user = useSupabaseUser()
-
-onMounted(() => {
-  watchEffect(() => {
-    if (user.value) {
-      navigateTo('home')
-    }
-  })
-})
-
-const darkMode = ref(false)
 const email = ref<string>('')
 const password = ref<string>('')
 
@@ -57,6 +41,7 @@ const submigRegister = async () => {
     return alert('El correo electrónico no es válido')
   }
   const [user, error] = await signUp(client, email.value, password.value)
+  console.log(user)
   console.log(error)
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div :class="{'dark': darkMode}">
-    <login />
+  <div class="bg-gray-50 dark:bg-slate-800 h-screen grid grid-cols-12" v-if="showAuth">
+    <login v-if="showLogin" @show-register="showLogin = false" />
+    <register v-else @show-login="showLogin = true" />
+    <auth-background />
+  </div>
+  <div v-else>
+    <h2>Bienvenido, haz iniciado sesión</h2>
   </div>
 </template>
 
 <script setup lang="ts">
-const darkMode = ref(false)
+import { User } from '@supabase/gotrue-js';
+import { Ref } from 'vue';
+
+const user: Ref<User> = useSupabaseUser();
+const showLogin = ref<boolean>(true);
+
+const showAuth = computed(() => {
+  return !user.value;
+})
 </script>
