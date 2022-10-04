9b72f632aff232071969278acbcc5aacf68f5430
Fercho5656 <morenomoranfernando@gmail.com>
2022-10-04 01:03:01 -0500

    Add default layout and auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-gray-50 dark:bg-slate-800">
+    <icons-campo-azul class="absolute m-3" />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-gray-50 dark:bg-slate-800 h-screen grid grid-cols-12">
+    <sidebar :routes="routes" />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { HomeIcon, ChartBarIcon, ChatBubbleOvalLeftEllipsisIcon, TicketIcon } from '@heroicons/vue/24/solid'
+
+const routes = [
+  {
+    name: 'Home',
+    icon: HomeIcon,
+    path: '/'
+  },
+  {
+    name: 'Analytics',
+    icon: ChartBarIcon,
+    path: '/analytics'
+  },
+  {
+    name: 'Tickets',
+    icon: TicketIcon,
+    path: '/tickets'
+  },
+  {
+    name: 'Chat',
+    icon: ChatBubbleOvalLeftEllipsisIcon,
+    path: '/chat'
+  }
+]
+
+</script>
+
+<style scoped>
+
+</style>
