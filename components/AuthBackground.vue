bc2ce355ab613637272a8926980684c3fb6e57c6
Fercho5656 <morenomoranfernando@gmail.com>
2022-10-04 01:03:18 -0500

    Remove div from nuxt img

--- components/AuthBackground.vue
+++ components/AuthBackground.vue
@@ -1,5 +1,3 @@
 <template>
-  <div class="hidden md:block col-span-5">
-    <nuxt-img class="h-full object-cover" src="/auth-bg.jpeg" placeholder alt="" />
-  </div>
+  <nuxt-img class="hidden md:block col-span-5 h-full object-cover" src="/auth-bg.jpeg" placeholder alt="" />
 </template>
