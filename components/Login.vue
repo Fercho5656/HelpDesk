25e650de9a74406e27614f9f73a46ec3f5d4cbca
Fercho5656 <morenomoranfernando@gmail.com>
2022-10-04 01:07:28 -0500

    Refactor components to fit in auth layout

--- components/Login.vue
+++ components/Login.vue
@@ -1,23 +1,18 @@
 <template>
-  <div class="flex flex-col col-span-12 md:col-span-7">
-    <nuxt-link class="p-3" to="/">
-      <icons-campo-azul />
-    </nuxt-link>
-    <form @submit.prevent="submitLogin"
-      class="w-full h-full flex flex-col self-center justify-center gap-y-12 md:w-9/12 p-5">
-      <h2 class="dark:text-gray-50 mb-3 text-2xl font-bold text-center">Bienvenido, inicie sesión</h2>
-      <div class="flex flex-col gap-y-10">
-        <u-i-input type="email" label="Correo Electrónico" placeholder="name@pfatequila.com" v-model="email" />
-        <u-i-password-input label="Contraseña" v-model="password" />
-      </div>
-      <button class="w-full py-3 dark:bg-cyan-900 bg-slate-800 text-gray-50 rounded-xl">Iniciar Sesión</button>
-      <p class="flex justify-center dark:text-gray-50">¿No tienes una cuenta?&nbsp; <nuxt-link to="#"
-          @click="$emit('show-register')"
-          class="text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300">Regístrate
-        </nuxt-link>
-      </p>
-    </form>
-  </div>
+  <form @submit.prevent="submitLogin"
+    class="flex flex-col w-full m-auto gap-y-12 col-span-12 md:col-span-7 md:w-9/12 p-5">
+    <h2 class="dark:text-gray-50 mb-3 text-2xl font-bold text-center">Bienvenido, inicie sesión</h2>
+    <div class="flex flex-col gap-y-10">
+      <u-i-input type="email" label="Correo Electrónico" placeholder="name@pfatequila.com" v-model="email" />
+      <u-i-password-input label="Contraseña" v-model="password" />
+    </div>
+    <button class="w-full py-3 dark:bg-cyan-900 bg-slate-800 text-gray-50 rounded-xl">Iniciar Sesión</button>
+    <p class="flex justify-center dark:text-gray-50">¿No tienes una cuenta?&nbsp; <nuxt-link to="#"
+        @click="$emit('show-register')"
+        class="text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300">Regístrate
+      </nuxt-link>
+    </p>
+  </form>
 </template>
 
 <script setup lang="ts">
