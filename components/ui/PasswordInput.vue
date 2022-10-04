36fb547916a86a1c26820315ff80a6da2a19ceef
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-04 16:45:01 -0500

    Removemargin and increase font size

--- components/ui/PasswordInput.vue
+++ components/ui/PasswordInput.vue
@@ -1,12 +1,12 @@
 <template>
-  <label class="relative mt-3 block text-sm font-medium text-gray-900 dark:text-gray-300">
-    <p class="absolute top-0 -translate-y-full">{{label}}</p>
+  <label class="relative block text-sm font-medium text-gray-900 dark:text-gray-300">
+    <p class="absolute top-0 -translate-y-full text-lg">{{label}}</p>
     <span class="absolute cursor-pointer top-1/2 w-7 h-7 -translate-y-1/2 left-3" @click="showPassword = !showPassword">
       <eye-slash-icon v-if="showPassword" />
       <eye-icon v-else />
     </span>
     <input
-      class="appearance-none block w-full py-3 px-4 pl-14 rounded-md bg-gray-200 dark:bg-gray-500 dark:placeholder-gray-300 focus:outline-none"
+      class="appearance-none block w-full py-3 px-4 pl-14 rounded-md text-lg bg-gray-200 dark:bg-gray-500 dark:placeholder-gray-300 focus:outline-none"
       :type="showPassword ? 'text' : 'password'" :placeholder="placeholder" :value="modelValue"
       @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)">
   </label>
