5fef57d08226bb953cdb6d5f78f8823527612a22
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-04 16:44:46 -0500

    Add icons for date and text type

--- components/ui/Input.vue
+++ components/ui/Input.vue
@@ -1,11 +1,13 @@
 <template>
   <label class="relative block text-sm font-medium text-gray-900 dark:text-gray-300">
-    <p class="absolute top-0 -translate-y-full">{{label}}</p>
+    <p class="absolute top-0 -translate-y-full text-lg">{{label}}</p>
     <span class="absolute pointer-events-none top-1/2 w-7 h-7 -translate-y-1/2 left-3">
       <envelope-icon v-if="type === 'email'" />
+      <user-icon v-if="type === 'text'"/>
+      <calendar-days-icon v-if="type === 'date'"/>
     </span>
     <input
-      class="appearance-none block w-full py-3 px-4 pl-14 rounded-md bg-gray-200 dark:bg-gray-500 dark:placeholder-gray-300 focus:outline-none"
+      class="appearance-none block w-full py-3 px-4 pl-14 text-lg rounded-md bg-gray-200 dark:bg-gray-500 dark:placeholder-gray-300 focus:outline-none"
       :type="type" :placeholder="placeholder" :value="modelValue"
       @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)">
   </label>
@@ -13,7 +15,7 @@
 
 <script setup lang="ts">
 
-import { EnvelopeIcon } from '@heroicons/vue/24/solid/'
+import { EnvelopeIcon, UserIcon, CalendarDaysIcon } from '@heroicons/vue/24/solid/'
 
 interface Props {
   label: string
