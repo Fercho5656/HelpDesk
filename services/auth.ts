17284a7f59ae77cfd87a875f7e752aa8f2c3a0e2
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-04 16:46:02 -0500

    Add data object to sign up user with fullname and birthday

--- services/auth.ts
+++ services/auth.ts
@@ -1,5 +1,12 @@
-import { ApiError, User } from "@supabase/gotrue-js"
+import { ApiError, User, Session } from "@supabase/gotrue-js"
 import { SupabaseClient } from "@supabase/supabase-js"
+
+interface IUserInfo {
+  fullName: string
+  email: string
+  password: string
+  birthday: string
+}
 
 // const client = useSupabaseClient()
 
@@ -8,7 +15,16 @@ export const login = async (client: SupabaseClient, email: string, password: str
   return [user, error]
 }
 
-export const signUp = async (client: SupabaseClient, email: string, password: string): Promise<[User, ApiError]> => {
-  const { user, error } = await client.auth.signUp({ email, password })
-  return [user, error]
+export const signUp = async (client: SupabaseClient, userInfo: IUserInfo): Promise<[User, Session, ApiError]> => {
+  const { user, session, error } = await client.auth.signUp({
+    email: userInfo.email,
+    password: userInfo.password,
+  },
+    {
+      data: {
+        fullName: userInfo.fullName,
+        birthday: userInfo.birthday,
+      },
+    })
+  return [user, session, error]
 }
