167a7d28f17ea6a29874996625603d90bce7ab29
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-07 11:49:16 -0500

    Add justify center in small screens

--- components/Sidebar/index.vue
+++ components/Sidebar/index.vue
@@ -2,7 +2,7 @@
   <aside class="hidden h-full col-span-4 md:col-span-3 xl:col-span-2 sm:flex flex-col p-5 border-r-[1px]">
     <icons-campo-azul class="mx-auto mb-10" />
     <ul class=" flex flex-col items-center md:items-start gap-y-5">
-      <li v-for="route in routes" :key="route.name" class="w-full hover:bg-slate-700 rounded-full transition-all">
+      <li v-for="route in routes" :key="route.name" class="w-full flex justify-center md:justify-start hover:bg-slate-700 rounded-full transition-all">
         <nuxt-link class="flex items-center gap-x-3 px-5 py-2" :to="route.path">
           <component class="w-10 h-10 fill-black dark:fill-white" :is="route.icon" />
           <p class="hidden md:block text-black dark:text-white text-xl">{{route.name}}</p>
@@ -26,7 +26,6 @@ interface Props {
     path: string
   }[]
 }
-
 
 defineProps<Props>()
 </script>
