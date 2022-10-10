c9623c263e909504ebf667c952f4d87dbcb13218
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-10 15:08:17 -0500

    Fix styles

--- components/ui/InputFile.vue
+++ components/ui/InputFile.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="flex items-center justify-center gap-x-5">
-    <div class="shrink-0">
+  <div class="flex items-center justify-center">
+    <div class="shrink-0 flex flex-col items-center">
+      <p class=" text-lg text-gray-300 text-center">Foto del Perfil</p>
       <img class="object-cover w-16 h-16 rounded-full" :src="profilePicUrl" alt="Foto del Perfíl" />
     </div>
-    <label class="block">
+    <label class="flex items-center justify-center">
       <span class="sr-only">Seleccionar Archivo</span>
       <input type="file"
         class="block w-full text-md text-gray-400 text-opacity-0 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-md file:font-semibold dark:file:bg-gray-500 dark:file:text-gray-300 hover:dark:file:bg-gray-600"
