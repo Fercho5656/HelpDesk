874352505be15f4b8cd8624a47ee1844db67be28
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-10 13:44:35 -0500

    Add select tag component

---
+++ components/ui/Select.vue
@@ -0,0 +1,18 @@
+<template>
+  <label class="relative block text-md font-medium text-gray-900 dark:text-gray-300">
+    <p class="absolute top-0 -translate-y-full text-md">{{label}}</p>
+    <select @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+      class="w-full py-3 px-4 text-md font-semibold dark:text-gray-200 bg-gray-200 dark:bg-gray-500 dark:placeholder-gray-300 rounded-md">
+      <slot />
+    </select>
+  </label>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label: string
+  modelValue: string | number
+}
+
+defineProps<Props>()
+</script>
