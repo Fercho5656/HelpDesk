fd918ba8850763f189bd4a15060bfd4c9fcb7319
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-10 13:44:54 -0500

    Fix select service

--- services/departments.ts
+++ services/departments.ts
@@ -1,9 +1,10 @@
 import IDepartment from "~~/interfaces/IDepartment"
-import { ApiError } from "@supabase/gotrue-js"
+import { PostgrestError, SupabaseClient } from "@supabase/supabase-js"
 
-const client = useSupabaseClient()
+export const getDepartments = async (client: SupabaseClient): Promise<[PostgrestError, IDepartment[]]> => {
+  const { error, data } = await client
+    .from<IDepartment>('department')
+    .select('*')
 
-export const getDepartments = async (): Promise<[IDepartment[], ApiError]> => {
-  const { data, error } = await client.from('departments').select('*')
-  return [data, error]
+  return [error, data]
 }
