230880b02dc7851a9d1a9c5d9d8c45a128cabb53
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-10 14:57:51 -0500

    Change default profile picture path

--- components/ui/InputFile.vue
+++ components/ui/InputFile.vue
@@ -6,7 +6,7 @@
     <label class="block">
       <span class="sr-only">Seleccionar Archivo</span>
       <input type="file"
-        class="block w-full text-md text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-md file:font-semibold dark:file:bg-gray-500 dark:file:text-gray-300"
+        class="block w-full text-md text-gray-400 text-opacity-0 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-md file:font-semibold dark:file:bg-gray-500 dark:file:text-gray-300 hover:dark:file:bg-gray-600"
         @change="onChange" />
     </label>
   </div>
@@ -21,7 +21,7 @@ interface Props {
 defineProps<Props>()
 const emits = defineEmits(['update:modelValue'])
 
-const profilePicUrl = ref<string>(".\\public\\default-profile-pic.png")
+const profilePicUrl = ref<string>("/default-profile-pic.png")
 
 const onChange = (e: Event): Event => {
   const file = (e.target as HTMLInputElement).files[0]
