7fae8931c33e18686e64ce1886dcbf975478c3cb
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-11 16:50:23 -0500

    Finish upload function

--- services/bucket/profilePic.ts
+++ services/bucket/profilePic.ts
@@ -1,12 +1,13 @@
 import { SupabaseClient } from "@supabase/supabase-js"
 
-export const uploadProfilePic = async (client: SupabaseClient, prefix: string, profilePic: File) => {
-  const { data, error: profilePicError } = await client
+export const uploadProfilePic = async (client: SupabaseClient, prefix: string, profilePic: File): Promise<[string, Error]> => {
+  const { data, error } = await client
     .storage
     .from('profile-pics')
     .upload(`${prefix}_profile_pic.png`, profilePic, {
       cacheControl: '3600',
       upsert: false
     })
-  if (profilePicError) return [null, null, profilePicError]
+  if (error) return ['', error]
+  return [data.Key, error]
 }
