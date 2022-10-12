5ded8e8013ad9dd378188f7450a5ec889114c007
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-12 16:49:07 -0500

    Add component to show a profile preview button that contains shortcuts

---
+++ components/Sidebar/AccountMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <sidebar-hover-card v-if="showHoverCard" :img="src" :username="firstName" />
+    <button @click="showHoverCard = !showHoverCard" class="rounded-full dark:hover:bg-gray-700 transition-all">
+      <sidebar-user-cell :img="src" :username="firstName" />
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  src: string
+  username: string
+}
+
+const props = defineProps<Props>()
+
+const firstName = computed(() => props.username.split(' ')[0])
+const showHoverCard = ref(true)
+</script>
+
+<style scoped>
+
+</style>
