fb9cb3d9788f3d4f68b8de4221d5d1b7dd00abce
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-12 16:47:47 -0500

    Add component to show profile picture and username

---
+++ components/Sidebar/UserCell.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-3 flex gap-x-3 justify-center items-center ">
+    <img class=" w-10 h-10 rounded-full" :src="img" alt="Foto del perfíl">
+    <p class="font-semibold tracking-wider dark:text-gray-200 text-left text-lg">{{ username }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  img: string
+  username: string
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped>
+
+</style>
