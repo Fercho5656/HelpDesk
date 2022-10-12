30508ec5ae5543c4cfaef0a7ec1031e82980ee80
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-12 16:48:05 -0500

    Add component to show profile options when click

---
+++ components/Sidebar/HoverCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class=" border rounded-3xl border-gray-50">
+    <sidebar-user-cell :img="img" :username="username" />
+    <hr>
+    <button class="p-3 w-full overflow-hidden dark:text-gray-200 font-semibold hover:dark:bg-gray-700">Cerrar Sesión</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  img: string
+  username: string
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped>
+
+</style>
