1cb9e8db3fd332d785bbb42a76a72178a906a72a
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-13 16:52:56 -0500

    Add basic state implementation

--- components/Sidebar/ChangeTheme.vue
+++ components/Sidebar/ChangeTheme.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="isDark = !isDark" class="p-3 w-full flex gap-x-3 justify-center items-center dark:hover:bg-gray-700 transition">
+  <button @click="changeTheme" class="p-3 w-full flex gap-x-3 justify-center items-center dark:hover:bg-gray-700 transition">
     <p class=" text-gray-700 dark:text-gray-300">Cambiar tema</p>
     <component class="w-8 h-8 fill-gray-800 dark:fill-gray-200" :is="darkIcon" />
   </button>
@@ -7,9 +7,20 @@
 
 <script setup lang="ts">
 import { SunIcon, MoonIcon } from '@heroicons/vue/24/solid'
+import { useUserSettings } from '~~/store/userSettings.store';
 
+const userSettings = useUserSettings()
 const isDark = ref<boolean>(true)
 const darkIcon = computed(() => isDark.value ? SunIcon : MoonIcon)
+
+onMounted(() => {
+  isDark.value = userSettings.getIsDarkMode
+})
+
+const changeTheme = () => {
+  isDark.value = !isDark.value
+  userSettings.setDarkMode(isDark.value)
+}
 
 </script>
 
