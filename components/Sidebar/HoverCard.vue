ff477b77112410e2cbc24989709022b02a914384
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-13 16:52:45 -0500

    Add hidden overflow style

--- components/Sidebar/HoverCard.vue
+++ components/Sidebar/HoverCard.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class=" border rounded-3xl border-gray-50">
+  <div class=" border rounded-3xl overflow-hidden w-fit border-gray-50">
     <sidebar-user-cell :img="img" :username="username" />
     <hr>
-    <button class="p-3 w-full overflow-hidden dark:text-gray-200 font-semibold hover:dark:bg-gray-700">Cerrar Sesión</button>
+    <sidebar-change-theme />
+    <hr>
+    <button class="p-3 w-full overflow-hidden dark:text-gray-200 font-semibold hover:dark:bg-gray-700 transition">
+      Cerrar Sesión
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
+
 interface Props {
   img: string
   username: string
