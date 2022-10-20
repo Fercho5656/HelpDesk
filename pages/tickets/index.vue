c6e862625c6288b818d3bc3e7ffdb7327b1bd8f7
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-20 16:47:26 -0500

    Add responsive grid column span

--- pages/tickets/index.vue
+++ pages/tickets/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Tickets</h1>
+  <div class="col-span-12 sm:col-span-8 md:col-span-9 xl:col-span-10">
+    <header class="w-full flex justify-around">
+      <h1 class=" text-5xl dark:text-gray-200">Tickets</h1>
+      <ui-button color="primary">Abrir&nbsp;Ticket</ui-button>
+    </header>
+  </div>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style scoped>
+
 </style>
