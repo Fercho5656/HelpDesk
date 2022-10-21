c9f574d4ad7dc3c1220abff37cbd4d83dec71ffe
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-21 16:44:14 -0500

    Add button to display priority help

--- pages/tickets/new.vue
+++ pages/tickets/new.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-10 col-span-12 sm:col-span-8 md:col-span-9 xl:col-span-10">
-    <h1 class="text-5xl">Nuevo Ticket</h1>
+    <h1 class="text-5xl dark:text-gray-200">Nuevo Ticket</h1>
     <form class="mt-10 flex flex-col gap-y-8">
       <ui-input type="text" label="Asunto" placeholder="Olvidé mi contraseña" />
       <ui-textarea type="textarea" label="Descripción" placeholder="Mi contraseña es: 123456" />
@@ -12,13 +12,17 @@
           </option>
         </template>
       </ui-select>
-      <ui-select label="Departamento">
-        <template v-for="department in departments">
-          <option :value="department.id">
-            {{department.name}}
-          </option>
-        </template>
-      </ui-select>
+      <fieldset class="flex items-center gap-x-10">
+        <ui-select class="w-full" label="Departamento">
+          <template v-for="department in departments">
+            <option :value="department.id">
+              {{department.name}}
+            </option>
+          </template>
+        </ui-select>
+        <ui-button type="primary">¿Qué prioridad elijo?</ui-button>
+      </fieldset>
+      <ui-button type="primary">Enviar</ui-button>
     </form>
   </div>
 </template>
