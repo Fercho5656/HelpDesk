d532edaae330aa8ca51a3af665195601b4bfdd28
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-21 16:23:53 -0500

    Remove logs

--- middleware/protectRoutes.global.ts
+++ middleware/protectRoutes.global.ts
@@ -1,7 +1,4 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  console.log('to', to)
-  console.log('from', from)
-
   const user = useSupabaseUser()
 
   if (user.value && to.name === 'auth') return navigateTo('/')
