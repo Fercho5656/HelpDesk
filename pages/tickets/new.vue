3cece6e69b6182fac50bf8ea31d99898d13054db
Fercho5656 <morenomoranfernando@gmail.com>
2022-10-22 18:49:12 -0500

    Add submit form event

--- pages/tickets/new.vue
+++ pages/tickets/new.vue
@@ -1,41 +1,64 @@
 <template>
   <div class="m-10 col-span-12 sm:col-span-8 md:col-span-9 xl:col-span-10">
     <h1 class="text-5xl dark:text-gray-200">Nuevo Ticket</h1>
-    <form class="mt-10 flex flex-col gap-y-8">
-      <ui-input type="text" label="Asunto" placeholder="Olvidé mi contraseña" />
-      <ui-textarea type="textarea" label="Descripción" placeholder="Mi contraseña es: 123456" />
-      <ui-input-file />
-      <ui-select label="Nivel de prioridad">
-        <template v-for="priority in ticketPriority">
-          <option :value="priority.id">
-            {{priority.priority}}
-          </option>
-        </template>
-      </ui-select>
+    <form @submit.prevent="onSubmitTicket" class="mt-10 flex flex-col gap-y-8">
+      <ui-input v-model="subject" type="text" label="Asunto" placeholder="Olvidé mi contraseña" />
+      <ui-textarea v-model="body" label="Descripción" placeholder="Mi contraseña es: 123456" />
+      <ui-input-file v-model="file" />
       <fieldset class="flex items-center gap-x-10">
-        <ui-select class="w-full" label="Departamento">
-          <template v-for="department in departments">
-            <option :value="department.id">
-              {{department.name}}
+        <ui-select v-model="priorityId" class="w-full" label="Nivel de prioridad">
+          <template v-for="priority in ticketPriority">
+            <option :value="priority.id">
+              {{priority.priority}}
             </option>
           </template>
         </ui-select>
-        <ui-button type="primary">¿Qué prioridad elijo?</ui-button>
+        <ui-button type="button" color="info">¿Qué prioridad elijo?</ui-button>
       </fieldset>
-      <ui-button type="primary">Enviar</ui-button>
+      <ui-select v-model="destinedDepartmentId" class="w-full" label="Departamento">
+        <template v-for="department in departments">
+          <option :value="department.id">
+            {{department.name}}
+          </option>
+        </template>
+      </ui-select>
+      <ui-button type="submit">Enviar</ui-button>
     </form>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { getPriorities } from '~/services/tickets/priorities'
 import { getDepartments } from "~/services/departments";
-const ticketPriority = ref([])
-const departments = ref([])
+import { sendTicket } from "~/services/tickets/ticket";
+import { PostgrestError } from '@supabase/postgrest-js';
+import ITicket from '~~/interfaces/ITicket';
+const ticketPriority = ref<any[] | PostgrestError>([])
+const departments = ref<any[] | PostgrestError>([])
+
+const subject = ref('')
+const body = ref('')
+const priorityId = ref(0)
+const destinedDepartmentId = ref(0)
+const file = ref()
+
 onBeforeMount(async () => {
   ticketPriority.value = await getPriorities()
   departments.value = await getDepartments()
 })
+
+const onSubmitTicket = async () => {
+  const user = useSupabaseUser()
+  const ticket: ITicket = {
+    subject: subject.value,
+    body: body.value,
+    priority_id: priorityId.value,
+    destined_department_id: destinedDepartmentId.value,
+    user_id: user.value.id,
+  }
+  const sentTicket = await sendTicket(ticket)
+  console.log(sentTicket)
+}
 </script>
 
 <style scoped>
