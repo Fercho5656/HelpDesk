36e6ca449881770cef04ba5e8eb0f4c85bf4dd26
Fercho5656 <morenomoranfernando@gmail.com>
2022-10-23 00:40:22 -0500

    Add value to select component

--- components/ui/Select.vue
+++ components/ui/Select.vue
@@ -1,7 +1,7 @@
 <template>
   <label class="relative block text-md font-medium text-gray-900 dark:text-gray-300">
     <p class="absolute top-0 -translate-y-full text-md">{{label}}</p>
-    <select @input="onInput"
+    <select @input="onInput" :value="modelValue"
       class="w-full py-3 px-4 text-md font-semibold dark:text-gray-200 bg-gray-200 dark:bg-gray-500 dark:placeholder-gray-300 rounded-md">
       <slot />
     </select>
