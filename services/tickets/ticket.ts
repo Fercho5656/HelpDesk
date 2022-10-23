5d2446caaec36f1031884b8b354a2e6f743b4c63
Fercho5656 <morenomoranfernando@gmail.com>
2022-10-23 01:41:46 -0500

    Add function to fetch all tickets

--- services/tickets/ticket.ts
+++ services/tickets/ticket.ts
@@ -15,3 +15,15 @@ export const sendTicket = async (ticket: ITicket): Promise<any[] | PostgrestErro
   }
   return data
 }
+
+export const getTickets = async (): Promise<any[] | PostgrestError> => {
+  const client = useSupabaseClient()
+
+  const { data, error } = await client
+    .from('ticket')
+    .select('*')
+  if (error) {
+    return error
+  }
+  return data
+}
