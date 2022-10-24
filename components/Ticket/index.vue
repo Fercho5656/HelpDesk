cfe78b6b5f2e0bdad7233f2aebe5df7b05f49b08
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-24 16:43:54 -0500

    Add ticket component

---
+++ components/Ticket/index.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+import ITicket from '~~/interfaces/ITicket';
+
+interface Props {
+  ticket: ITicket
+}
+</script>
+
+<style scoped>
+</style>
