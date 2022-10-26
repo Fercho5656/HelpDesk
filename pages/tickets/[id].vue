7817bb419d073a81e63684dadbfc2c287c113e67
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-26 16:38:27 -0500

    Add page to show conversation for a single ticket

---
+++ pages/tickets/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  {{ route.params.id }}
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+definePageMeta({
+  middleware: ['check-ticket']
+})
+</script>
+
+<style scoped>
+
+</style>
