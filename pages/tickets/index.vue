29203597e7a1c633cd2cca84a3ef6de0e30d1181
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-26 13:39:52 -0500

    Show tickets

--- pages/tickets/index.vue
+++ pages/tickets/index.vue
@@ -9,17 +9,18 @@
       </ui-button>
     </header>
     <nav class="flex justify-center mt-5 items-center gap-x-5">
-      <ui-select class="md:hidden" v-model="ticketStatusView">
-        <template v-for="status in ticketStatus" :key="status.name">
-          <option :value="status.value">
-            {{status.name}}
+      <ui-select class="md:hidden" v-model.number="activeAnchorIndex">
+        <template v-for="(status, index) in ticketStatus" :key="status.id">
+          <option :value="index">
+            {{ status.name }}
           </option>
         </template>
       </ui-select>
       <ul class="hidden w-full md:flex gap-x-5 justify-start">
         <template v-for="(status, index) in ticketStatus" :key="status.name">
-          <a :href="`#${status.value}`" :class="activeAnchorIndex === index ? 'border-b-2 border-red-500 pb-0.5' : ''" @click="activeAnchorIndex = index">
-            <li class="md:text-lg lg:text-2xl truncate dark:text-gray-200">{{status.name}}</li>
+          <a :href="`#${status.value}`" :class="activeAnchorIndex === index ? 'border-b-2 border-red-500 pb-0.5' : ''"
+            @click="activeAnchorIndex = index">
+            <li class="md:text-lg lg:text-2xl truncate dark:text-gray-200">{{ status.name }}</li>
           </a>
         </template>
       </ul>
@@ -27,23 +28,30 @@
       <!-- Filter Button -->
     </nav>
     <main class="mt-10">
-      
+      <div class=" flex flex-col gap-y-5">
+        <template v-for="ticket in ticketsToShow.value" :key="ticket.id">
+          <ticket :ticket="ticket" />
+        </template>
+      </div>
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ComputedRef } from 'vue';
+import ITicket from '~~/interfaces/ITicket';
+
 
 const searchTicket = ref<string>('')
 const ticketStatusView = ref<string>('')
 const activeAnchorIndex = ref<number>(0)
-const ticketsToShow = computed(() => {
+const ticketsToShow = computed((): ComputedRef<ITicket[]> => {
   if (activeAnchorIndex.value === 0) return openTickets
   if (activeAnchorIndex.value === 1) return assignedToMeTickets
-  if (activeAnchorIndex.value === 2) return tickets
+  if (activeAnchorIndex.value === 2) return allTickets
   if (activeAnchorIndex.value === 3) return closedTickets
 })
-const { tickets, openTickets, assignedToMeTickets, closedTickets } = useTickets()
+const { allTickets, openTickets, assignedToMeTickets, closedTickets } = useTickets()
 
 const ticketStatus = [
   {
