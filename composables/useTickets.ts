244ad6908d58d93fc3083a6e581903b3b241eb06
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-10-26 13:39:20 -0500

    Add ticket interface as type return

--- composables/useTickets.ts
+++ composables/useTickets.ts
@@ -1,4 +1,5 @@
 import { PostgrestError } from '@supabase/supabase-js'
+import ITicket from '~~/interfaces/ITicket'
 import { getTickets } from '~~/services/tickets/ticket'
 
 const STATUS = {
@@ -11,7 +12,7 @@ const STATUS = {
 
 export const useTickets = () => {
   const user = useSupabaseUser()
-  const tickets = useState<any[] | PostgrestError>('tickets', () => [])
+  const tickets = useState<ITicket[] | PostgrestError>('tickets', () => [])
 
   const fetchTickets = async () => {
     if (!user) return
@@ -21,22 +22,27 @@ export const useTickets = () => {
   fetchTickets()
 
   const openTickets = computed(() => {
-    const ticketsArray = tickets.value as any[]
+    const ticketsArray = tickets.value as ITicket[]
     return ticketsArray.filter((ticket) => ticket.status_id = STATUS.OPEN)
   })
 
   const assignedToMeTickets = computed(() => {
-    const ticketsArray = tickets.value as any[]
-    return ticketsArray.filter((ticket: any) => ticket.user_attending_id === user.value?.id)
+    const ticketsArray = tickets.value as ITicket[]
+    return ticketsArray.filter((ticket: ITicket) => ticket.user_attending_id === user.value?.id)
   })
 
   const closedTickets = computed(() => {
-    const ticketsArray = tickets.value as any[]
-    return ticketsArray.filter((ticket: any) => ticket.status_id === STATUS.CLOSED)
+    const ticketsArray = tickets.value as ITicket[]
+    return ticketsArray.filter((ticket: ITicket) => ticket.status_id === STATUS.CLOSED)
   })
 
+  const allTickets = computed(() => {
+    return tickets.value as ITicket[]
+  })
+
+
   return {
-    tickets,
+    allTickets,
     openTickets,
     assignedToMeTickets,
     closedTickets,
