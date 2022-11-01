7776ddf04ff1af9f1ba6e852c1451677f5d524b0
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-01 15:31:01 -0600

    Add buttons and behaviours for every ticket status

--- components/Ticket/Status/BadgeControl.vue
+++ components/Ticket/Status/BadgeControl.vue
@@ -1,7 +1,19 @@
 <template>
-  <ticket-status-button @click="openTicket" :class="actualStatus.style" v-if="actualStatus.id === 1" @mouseover="hover = true"
-    @mouseleave="hover = false">
+  <ticket-status-button @click="openTicket" :class="actualStatus.style" v-if="actualStatus.id === 1"
+    @mouseover="hover = true" @mouseleave="hover = false">
     <p>{{ hover ? 'Tomar Ticket' : 'Abierto' }}</p>
+  </ticket-status-button>
+  <ticket-status-button @click="deliverTicket" :class="actualStatus.style" v-if="actualStatus.id === 2"
+    @mouseover="hover = true" @mouseleave="hover = false">
+    <p>{{ hover ? 'Entregar Ticket' : 'En Proceso' }}</p>
+  </ticket-status-button>
+  <ticket-status-button @click="deliverTicket" :class="actualStatus.style" v-if="actualStatus.id === 3"
+    @mouseover="hover = true" @mouseleave="hover = false">
+    <p>{{ hover ? 'Cerrar Ticket' : 'Cerrado' }}</p>
+  </ticket-status-button>
+  <ticket-status-button class="pointer-events-none" :class="actualStatus.style"
+    v-if="actualStatus.id === 4 || actualStatus.id === 5">
+    <p>{{ actualStatus.name }}</p>
   </ticket-status-button>
 </template>
 
@@ -33,30 +45,36 @@ const STATUS = [
     id: 4,
     name: 'Entregado',
     color: 'yellow',
-    style: 'bg-yellow-600 dark:bg-yellow-300 text-yellow-50 dark:text-yellow-800 hover:bg-yellow-700 dark:hover:bg-yellow-400'
+    style: 'bg-yellow-600 dark:bg-yellow-300 text-yellow-50 dark:text-yellow-800'
   },
   {
     id: 5,
     name: 'Cerrado',
     color: 'red',
-    style: 'bg-red-600 dark:bg-red-300 text-red-50 dark:text-red-800 hover:bg-red-700 dark:hover:bg-red-400'
+    style: 'bg-red-600 dark:bg-red-300 text-red-50 dark:text-red-800'
   }
 ]
 
 const props = defineProps<Status>()
-const emits = defineEmits(['update:status'])
+const emits = defineEmits(['update:status', 'take-ticket'])
 const hover = ref(false)
 const actualStatus = ref(STATUS.find((s: any) => s.id === props.status))
 
 const openTicket = () => {
   if (confirm('¿Estás seguro de tomar este ticket?')) {
+    hover.value = false
     actualStatus.value = STATUS.find((s: any) => s.id === 2)
+    emits('take-ticket')
     emits('update:status', 2)
   }
 }
 
 const deliverTicket = () => {
-
+  if (confirm('¿Estás seguro de entregar este ticket al usuario?')) {
+    hover.value = false
+    actualStatus.value = STATUS.find((s: any) => s.id === 4)
+    emits('update:status', 4)
+  }
 }
 </script>
 
