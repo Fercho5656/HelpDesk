3fe701c605ce7cb42572f84af11865fce4847683
Fercho5656 <morenomoranfernando@gmail.com>
2022-11-07 01:31:07 -0600

    Make twilio create conversation logic for attendant user

--- pages/tickets/[id].vue
+++ pages/tickets/[id].vue
@@ -1,18 +1,29 @@
 <template>
-  <header class="w-full flex items-center justify-start gap-x-5">
-    <h1 class="dark:text-gray-100 text-5xl font-semibold"> {{ ticket.subject }} </h1>
-    <div class="flex items-center gap-x-3">
-      <ticket-priority-badge :priority="ticket.priority_id" :is-editable="showEditablePriority" />
-      <ticket-status-badge :status="ticket.status_id" v-if="statusComponent" />
-      <ticket-status-badge-control @take-ticket="onTakeTicket" @update:status="onUpdateStatus"
-        :status="ticket.status_id" v-else />
-    </div>
-  </header>
+  <div>
+    <header class="w-full flex items-center justify-start gap-x-5">
+      <h1 class="dark:text-gray-100 text-5xl font-semibold"> {{ ticket.subject }} </h1>
+      <div class="flex items-center gap-x-3">
+        <ticket-priority-badge :priority="ticket.priority_id" :is-editable="showEditablePriority" />
+        <ticket-status-badge :status="ticket.status_id" v-if="statusComponent" />
+        <ticket-status-badge-control @take-ticket="onTakeTicket" @update:status="onUpdateStatus"
+          :status="ticket.status_id" v-else />
+      </div>
+    </header>
+    <main>
+      {{ conversationMessages }}
+      <!-- <conversation :messages="conversationMessages" /> -->
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { Message } from '@twilio/conversations';
+import { Client } from 'twilio-sync';
 import ITicket from '~~/interfaces/ITicket';
-import { updateStatus, updateAttendantUser } from '~~/services/tickets/ticket';
+import { addConversation } from '~~/services/conversation';
+import { updateStatus, updateAttendantUser, updateConversationId } from '~~/services/tickets/ticket';
+import { createConversation, getConversation, joinConversation } from '~~/services/twilio/conversation';
+import { useConversationStore } from '~~/store/conversation.store';
 
 definePageMeta({
   middleware: ['check-ticket']
@@ -21,22 +32,58 @@ definePageMeta({
 const route = useRoute()
 const user = useSupabaseUser()
 const client = useSupabaseClient()
+const conversationStore = useConversationStore()
 const ticket = ref<ITicket>(await useTicket(route.params.id as string) as ITicket)
-// const ticket: ITicket = await useTicket(route.params.id as string) as ITicket
 
 // If the user is the owner of the ticket, show the status badge else show the status badge control
 const statusComponent = computed(() => (ticket.value.user_id === user.value.id))
-
 const showEditablePriority = computed(() => (ticket.value.user_attending_id === user.value.id))
+const showConversationNotAvailable = computed(() => (ticket.value.conversation_id == null))
+
+const conversationMessages = ref<Array<Message>>([])
+
+onMounted(async () => {
+  if (ticket.value.conversation_id == null) return
+  // const conversation = await getConversation(conversationStore.getTwilioAccessToken, ticket.value.conversation.twilio_conversation_SID)
+    const conversation = await getConversation(ticket.value.conversation.twilio_conversation_SID, conversationStore.getTwilioAccessToken)
+    const messages = await conversation.getMessages()
+    //@ts-ignore
+    conversationMessages.value = messages.items
+    console.log(conversationMessages.value)
+})
 
 const onUpdateStatus = (newStatus: number) => {
   ticket.value.status_id = newStatus
   updateStatus(ticket.value.id, newStatus)
 }
 
-const onTakeTicket = () => {
+const onTakeTicket = async () => {
   ticket.value.user_attending_id = user.value.id
-  updateAttendantUser(ticket.value.id, user.value.id)
+  await updateAttendantUser(ticket.value.id, user.value.id)
+
+  const conversationName = `${ticket.value.user_id}-${ticket.value.subject}-${Date.now()}`
+  const accessToken: string = conversationStore.getTwilioAccessToken
+  const conversation = await createConversation(conversationName, accessToken)
+
+  try {
+    const createParticipant = useFetch('/api/twilio/createparticipant', {
+      method: 'POST',
+      body: JSON.stringify({
+        conversationSID: conversation.sid,
+        identity: ticket.value.user_id
+      })
+    })
+  } catch (error) {
+    await conversation.add(user.value.id)
+  }
+
+
+  const dbConversation = await addConversation({
+    twilio_conversation_SID: conversation.sid,
+    user_creator_id: ticket.value.user_id
+  })
+
+  await updateConversationId(ticket.value.id, dbConversation[0].id)
 }
 
 const updateSuscription = client
