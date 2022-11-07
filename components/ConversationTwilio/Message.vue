27d0fd74f05b9a63d4947138510aab36f1f33003
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-07 16:49:07 -0600

    Add component to show a single message

---
+++ components/ConversationTwilio/Message.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="isAuthor ? 'bg-emerald-800 text-right' : 'bg-gray-700 text-left' " class="flex flex-col rounded-md text-gray-200 p-3">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  isAuthor: boolean
+}
+
+const props = defineProps<Props>()
+</script>
+
+<style scoped>
+
+</style>
