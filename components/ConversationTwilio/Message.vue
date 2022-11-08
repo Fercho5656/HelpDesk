5578377fb67d5bccee8011889dc05499e4703eb9
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-08 16:51:24 -0600

    Fix message structure, alignment and styles

--- components/ConversationTwilio/Message.vue
+++ components/ConversationTwilio/Message.vue
@@ -1,15 +1,24 @@
 <template>
-  <div :class="isAuthor ? 'bg-emerald-800 text-right' : 'bg-gray-700 text-left' " class="flex flex-col rounded-md text-gray-200 p-3">
-    <slot />
+  <div class="flex flex-col" :class="isAuthor ? 'self-end' : 'self-start'">
+    <div :class="isAuthor ? 'bg-emerald-800 self-end rounded-tr-none' : 'bg-gray-700 self-start rounded-tl-none'" class="relative w-fit rounded-md text-gray-200 p-3">
+      <icons-message-arrow class="absolute top-0" :class="isAuthor ? '-right-2 -scale-x-100 text-emerald-800' : '-left-2 text-gray-700'" />
+      <p> {{ message.body }}</p>
+    </div>
+    <p :class="isAuthor ? 'self-end' : 'self-start'" class="text-gray-200">{{ message.author }} {{date}}</p>
   </div>
 </template>
 
 <script setup lang="ts">
+import IMessage from '~~/interfaces/IMessage';
+
 interface Props {
   isAuthor: boolean
+  message: IMessage
 }
 
 const props = defineProps<Props>()
+
+const date = computed(() => new Date(props.message.dateCreated).toDateString())
 </script>
 
 <style scoped>
--- components/ConversationTwilio/index.vue
+++ components/ConversationTwilio/index.vue
@@ -1,22 +1,23 @@
 <template>
-  <div class="flex flex-col gap-y-3 w-full">
-    <conversation-twilio-message v-for="message in messages" :key="message.sid" :is-author="message.author === userId">
-      <p>{{ message.author }}</p>
-      <p>{{ message.body }}</p>
-    </conversation-twilio-message>
+  <div class="flex flex-col gap-y-3 w-full p-3 overflow-y-scroll">
+    <conversation-twilio-message v-for="message in messages" :key="message.sid" :is-author="message.author === userId"
+      :message="message" />
   </div>
 </template>
 
 <script setup lang="ts">
 import IMessage from '~~/interfaces/IMessage'
 import { useUserStore } from '~~/store/user.store';
-const userStore = useUserStore()
-const userId = computed(() => userStore.getUserId)
+
 interface Props {
   messages: Array<IMessage>
 }
 
 const props = defineProps<Props>()
+
+const userStore = useUserStore()
+const userId = computed(() => userStore.getUserId)
+
 console.log(props.messages)
 </script>
 
