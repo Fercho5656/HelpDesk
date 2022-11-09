471530e22a888b3022b0baf2433eb969cdd4987d
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-09 16:46:49 -0600

    Add custom height properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
       transitionProperty: {
         'height': 'height',
         'width': 'width',
+      },
+      height: {
+        'screen/2': '50vh',
+        'screen/3': '33.333333vh',
+        'screen/4': '25vh',
+        'screen/5': '20vh',
       }
     },
   },
