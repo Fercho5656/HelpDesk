c9a9287ee69864ea664c8820fc05d324d45552d7
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-09 16:50:01 -0600

    Change non author message background color

--- components/ConversationTwilio/Message.vue
+++ components/ConversationTwilio/Message.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col" :class="isAuthor ? 'self-end' : 'self-start'">
-    <div :class="isAuthor ? 'bg-emerald-800 self-end rounded-tr-none' : 'bg-gray-700 self-start rounded-tl-none'" class="relative w-fit rounded-md text-gray-200 p-3">
-      <icons-message-arrow class="absolute top-0" :class="isAuthor ? '-right-2 -scale-x-100 text-emerald-800' : '-left-2 text-gray-700'" />
+    <div :class="isAuthor ? 'bg-emerald-800 self-end rounded-tr-none' : 'bg-gray-800 self-start rounded-tl-none'" class="relative w-fit rounded-md text-gray-200 p-3">
+      <icons-message-arrow class="absolute top-0" :class="isAuthor ? '-right-2 -scale-x-100 text-emerald-800' : '-left-2 text-gray-800'" />
       <p> {{ message.body }}</p>
     </div>
     <p :class="isAuthor ? 'self-end' : 'self-start'" class="text-gray-200">{{ message.author }} {{date}}</p>
