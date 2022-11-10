e38ac285d39e296f1c639279a04fe6f5c2086024
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-10 12:41:53 -0600

    Remove cursor behaviour when badge is not editable

--- components/Ticket/PriorityBadge.vue
+++ components/Ticket/PriorityBadge.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative">
-    <button ref="priorityRef" :class="actualPriority.style"
+    <button ref="priorityRef" :class="`${actualPriority.style} ${isEditable ? '' : 'pointer-events-none'} `"
       class="inline-flex items-center rounded-full p-2 px-5 font-bold tracking-wide" @click="showList">
       <p>
         {{ actualPriority.name }}
@@ -30,35 +30,39 @@ interface Props {
 const props = defineProps<Props>()
 const emits = defineEmits(['update:priority'])
 
-const PRIORITIES = {
-  1: {
+const PRIORITIES = [
+  {
+    id: 1,
     name: 'Baja',
     color: 'green',
     style: 'bg-green-600 dark:bg-green-300 text-green-50 dark:text-green-800 hover:bg-green-700 dark:hover:bg-green-400'
   },
-  2: {
+  {
+    id: 2,
     name: 'Media',
     color: 'blue',
     style: 'bg-blue-600 dark:bg-blue-300 text-blue-50 dark:text-blue-800 hover:bg-blue-700 dark:hover:bg-blue-400'
   },
-  3: {
+  {
+    id: 3,
     name: 'Alta',
     color: 'yellow',
     style: 'bg-yellow-600 dark:bg-yellow-300 text-yellow-50 dark:text-yellow-800 hover:bg-yellow-700 dark:hover:bg-yellow-400'
   },
-  4: {
+  {
+    id: 4,
     name: 'Urgente',
     color: 'red',
     style: 'bg-red-600 dark:bg-red-300 text-red-50 dark:text-red-800 hover:bg-red-700 dark:hover:bg-red-400'
   }
-}
+]
 
-const actualPriority = ref(PRIORITIES[props.priority])
+const actualPriority = ref(PRIORITIES.find((priority) => priority.id === props.priority))
 const priorityRef = ref(null)
 const showPriorities = ref<boolean>(false)
 
 watch(() => props.priority, (newPriority) => {
-  actualPriority.value = PRIORITIES[newPriority]
+  actualPriority.value = PRIORITIES.find((priority) => priority.id === newPriority)
 })
 
 onClickOutside(priorityRef, () => {
@@ -68,8 +72,8 @@ onClickOutside(priorityRef, () => {
 const changePriority = (newPriority: string) => {
   if (newPriority === actualPriority.value.name) return
   if (confirm(`¿Estás seguro de cambiar la prioridad a ${newPriority.toLocaleLowerCase()}?`)) {
-    actualPriority.value = Object.values(PRIORITIES).find((p: any) => p.name === newPriority)
-    emits('update:priority', Object.keys(PRIORITIES).find((p: any) => p === newPriority))
+    // actualPriority.value = Object.values(PRIORITIES).find((p: any) => p.name === newPriority)
+    emits('update:priority', Object.values(PRIORITIES).find((p: any) => p.name === newPriority).id)
   }
 }
 
