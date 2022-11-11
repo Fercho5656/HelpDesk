a39fa35dbca075d5b5cfb3c1dbb0472752d882ee
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-11 12:52:20 -0600

    Improve responsivity

--- pages/tickets/index.vue
+++ pages/tickets/index.vue
@@ -9,7 +9,7 @@
           </a>
         </ui-button>
       </div>
-      <nav class="flex justify-center mt-5 items-center gap-x-5">
+      <nav class="flex justify-between w-full mt-5 items-center gap-x-5">
         <ui-select class="md:hidden" v-model.number="activeAnchorIndex">
           <template v-for="(status, index) in ticketStatus" :key="status.id">
             <option :value="index">
@@ -21,7 +21,7 @@
           <template v-for="(status, index) in ticketStatus" :key="status.name">
             <a :href="`#${status.value}`" :class="activeAnchorIndex === index ? 'border-b-2 border-red-500 pb-0.5' : ''"
               @click="activeAnchorIndex = index">
-              <li class="md:text-lg lg:text-2xl truncate dark:text-gray-200">{{ status.name }}</li>
+              <li class="md:text-lg lg:text-xl truncate dark:text-gray-200">{{ status.name }}</li>
             </a>
           </template>
         </ul>
