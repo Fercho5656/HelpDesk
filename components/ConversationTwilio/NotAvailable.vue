3d584b97c608cde3715a291c9b518ba8bc6ea7f3
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-11 16:54:20 -0600

    Add component to display when a ticket is not available

---
+++ components/ConversationTwilio/NotAvailable.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="absolute w-full h-full p-14 flex flex-col items-center justify-center gap-y-10 z-10 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <p class="text-3xl w-full text-left font-semibold dark:text-gray-200">Este ticket aun no está siendo atendido</p>
+    <p class="text-3xl w-full text-left dark:text-gray-200">Por favor, espere a que algún miembro del personal lo tome</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
