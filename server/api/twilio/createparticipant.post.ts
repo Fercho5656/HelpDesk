19b9b3b6e4e87681b5b23049d1de6a7fa6c7e7b2
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-11 13:04:41 -0600

    Create participant directly to service instead of conversation

--- server/api/twilio/createparticipant.post.ts
+++ server/api/twilio/createparticipant.post.ts
@@ -2,20 +2,23 @@ import Client from 'twilio'
 
 export default defineEventHandler(async (event) => {
   const body = await useBody(event)
-  const { identity, conversationSID } = body
+  const { identity, friendlyName, conversationSID } = body
   const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_SERVICE_SID } = process.env
   //console.log('TWILIO_ACCOUNT_SID', TWILIO_ACCOUNT_SID)
   //console.log('TWILIO_AUTH_TOKEN', TWILIO_AUTH_TOKEN)
   const client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
   console.log(client.conversations.v1.users)
-  const addedParticipant = await client.conversations.v1.services(TWILIO_SERVICE_SID)
+
+  /* const addedParticipant = await client.conversations.v1.services(TWILIO_SERVICE_SID)
     .conversations(conversationSID)
     .participants
-    .create({ identity: identity })
+    .create({ identity: identity }) */
 
-  /* const addedParticipant = await client.conversations.v1.conversations(conversationSID)
-    .participants
-    .create({ identity }) */
+  const addedParticipant = await client.conversations.v1.services(TWILIO_SERVICE_SID)
+    .users
+    .create({ identity: identity, friendlyName: friendlyName })
+
+  console.log(addedParticipant)
 
   return addedParticipant
 })
