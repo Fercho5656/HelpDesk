ff5f9d79240c255430d9f7e0115b2bf94ea8ad57
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-11 13:00:47 -0600

    Add placebo dark mode

--- composables/useToast.ts
+++ composables/useToast.ts
@@ -3,8 +3,12 @@ import 'simple-notify/dist/simple-notify.min.css'
 import { IArgs } from 'simple-notify/dist/types'
 
 export const useToast = (toastParameters: IArgs) => {
+  const isDark = useDark()
+  const LIGHT_TYPE = 1
+  const DARK_TYPE = 2
   const notify = new Notify({
-    ...toastParameters
+    ...toastParameters,
+    type: isDark ? DARK_TYPE : LIGHT_TYPE,
   })
   return notify
 }
