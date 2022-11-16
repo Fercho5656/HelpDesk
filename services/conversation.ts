6a23b124a49a7447622fe791b25d2eefd912bc82
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-16 16:52:04 -0600

    Add function to fetch a single conversation on DB

--- services/conversation.ts
+++ services/conversation.ts
@@ -12,3 +12,15 @@ export const addConversation = async (conversation: IConversation): Promise<any[
   }
   return data
 }
+
+export const getDatabaseConversation = async (conversationId: number): Promise<any[] | PostgrestError> => {
+  const client = useSupabaseClient()
+  const { data, error } = await client
+    .from('conversation')
+    .select()
+    .eq('id', conversationId)
+  if (error) {
+    return error
+  }
+  return data
+}
