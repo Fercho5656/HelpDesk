31ece57daebf5b95939a83fa0997f7bc4ed21eb9
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-11-17 14:33:16 -0600

    Add message type

--- composables/useTwilioMessages.ts
+++ composables/useTwilioMessages.ts
@@ -1,12 +1,13 @@
+import { Message } from "@twilio/conversations"
 import IMessage from "~~/interfaces/IMessage"
 import { getConversation } from "~~/services/twilio/conversation"
 
 export const useTwilioMessages = async (twilioConversationSID: string, twilioAccessToken: string) => {
   const conversation = await getConversation(twilioConversationSID, twilioAccessToken)
   const messages = await conversation.getMessages()
-  const messagesArray = messages.items.map((message: any) => {
+  const messagesArray = messages.items.map((message: Message) => {
     const { attachedMedia, attributes, author, body, dateCreated, dateUpdated,
-      index, lastUpdatedBy, media, participantSid, sid, subject, type } = message
+      index, lastUpdatedBy, media, participantSid, sid, subject, type,} = message
     return {
       attachedMedia, attributes, author, body, dateCreated, dateUpdated,
       index, lastUpdatedBy, media, participantSid, sid, subject, type
