59342365fdb1fe7b25000b79850b4043e170c437
Fercho5656 <morenomoranfernando@gmail.com>
2022-12-06 00:32:27 -0600

    Add conditional to return ticket if it's closed

--- composables/useTicket.ts
+++ composables/useTicket.ts
@@ -5,6 +5,7 @@ export const useTicket = async (id: string) => {
   const user = useSupabaseUser()
   const ticket = await getTicket(id) as ITicket
 
+  if (ticket.status_id === 5) return ticket
   if (ticket.user_id === user.value.id) return ticket
   if (ticket.user_attending_id !== null) {
     if (ticket.user_attending_id === user.value.id) return ticket
