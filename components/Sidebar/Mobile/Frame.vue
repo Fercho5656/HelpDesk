7026fe1df4df257f75419a1eec67caf92cb6c139
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-12-06 16:53:56 -0600

    Create sidebar for mobile devices

---
+++ components/Sidebar/Mobile/Frame.vue
@@ -0,0 +1,23 @@
+<template>
+  <nuxt-link :to="props.route.path">
+    <component class="w-10 h-10 fill-black dark:fill-white" :is="props.route.icon">
+      <p class="text-black dark:text-white text-xl">{{ props.route.name }}</p>
+    </component>
+  </nuxt-link>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  route: {
+    name: string
+    icon: any
+    path: string
+  }
+}
+
+const props = defineProps<Props>()
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/Sidebar/Mobile/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full flex justify-around">
+    <sidebar-mobile-frame v-for="route in props.routes" :key="route.name" :route="route" />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  routes: {
+    name: string
+    icon: any
+    path: string
+  }[]
+}
+
+const props = defineProps<Props>()
+</script>
+
+<style scoped>
+
+</style>
