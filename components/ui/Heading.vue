122cf1fcf39673844fb0652dc344e7effbe56939
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-12-07 16:51:33 -0600

    Add component for headings

---
+++ components/ui/Heading.vue
@@ -0,0 +1,30 @@
+<template>
+  <component :is="props.heading" class="font-semibold" :class="textStyles">
+    <slot />
+  </component>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  heading: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+}
+
+const props = defineProps<Props>()
+
+const textSizes = {
+  h1: 'text-5xl',
+  h2: 'text-4xl',
+  h3: 'text-3xl',
+  h4: 'text-2xl',
+  h5: 'text-xl',
+  h6: 'text-lg',
+}
+
+const textStyles = computed(() => {
+  return textSizes[props.heading]
+})
+</script>
+
+<style scoped>
+
+</style>
