c7d6efdd29c956c80d20dfca401d082265e06755
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-12-26 16:45:46 -0600

    Add service to fetch tickets analytics

---
+++ services/analytics/tickets.ts
@@ -0,0 +1,18 @@
+import { serverSupabaseClient } from "#supabase/server"
+import { CompatibilityEvent } from "h3"
+
+export const totalTicketCountByPriority = async (event: CompatibilityEvent) => {
+  const client = serverSupabaseClient(event)
+  const { data, error } = await client
+    .from('total_open_tickets_by_priority')
+    .select('*')
+    .single()
+  return { data, error }
+}
+
+export const ticketCountByPriority = async (event: CompatibilityEvent, startDate?: string, finalDate?: string) => {
+  const client = serverSupabaseClient(event)
+  const { data, error } = await client
+    .rpc('open_tickets_by_priority', { "start_date": startDate, "final_date": finalDate })
+  return { data, error }
+}
