4d41a4ef691adebff01fe9fed5fdeb6519c8d534
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-12-28 16:50:14 -0600

    Add function type return

--- services/analytics/tickets.ts
+++ services/analytics/tickets.ts
@@ -1,5 +1,7 @@
 import { serverSupabaseClient } from "#supabase/server"
 import { CompatibilityEvent } from "h3"
+import ISupabaseQuery from "~~/interfaces/ISupabaseQuery"
+import ITicketCount from "~~/interfaces/ITicketCount"
 
 export const totalTicketCountByPriority = async (event: CompatibilityEvent) => {
   const client = serverSupabaseClient(event)
@@ -10,7 +12,7 @@ export const totalTicketCountByPriority = async (event: CompatibilityEvent) => {
   return { data, error }
 }
 
-export const ticketCountByPriority = async (event: CompatibilityEvent, startDate?: string, endDate?: string) => {
+export const ticketCountByPriority = async (event: CompatibilityEvent, startDate?: string, endDate?: string): Promise<ISupabaseQuery<ITicketCount>> => {
   const client = serverSupabaseClient(event)
   const { data, error } = await client
     .rpc('open_tickets_by_priority', { "start_date": startDate, "end_date": endDate })
