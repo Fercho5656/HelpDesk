6b4dda4aca7d5c2a5001ab3bb790bc1205324bad
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-12-28 16:49:16 -0600

    Add bar chart

--- pages/analytics.vue
+++ pages/analytics.vue
@@ -1,9 +1,16 @@
 <template>
-  <ui-heading heading="h1">Analytics</ui-heading>
-  
+  <div>
+    <ui-heading heading="h1">Analytics</ui-heading>
+    <bar :data="ticketCountByPriorityData" :options="ticketCountByPriorityOptions" />
+    <!-- <bar :data="data" :options="options" /> -->
+  </div>
 </template>
 
 <script setup lang="ts">
+import { Bar } from 'vue-chartjs'
+
+const { ticketCountByPriorityData, ticketCountByPriorityOptions } = await useAnalytics()
+
 </script>
 
 <style scoped>
