b680c6a572563e50cc16f50d378ce98c50ba381c
Fernando Moreno Moran <morenomoranfernando@gmail.com>
2022-12-29 16:15:42 -0600

    Make composable correctly asynchronous

--- composables/useAnalytics.ts
+++ composables/useAnalytics.ts
@@ -7,42 +7,60 @@ import {
   CategoryScale,
   LinearScale,
   ChartData,
-  ChartOptions
+  ChartOptions,
 } from 'chart.js'
 
-export default async function useAnalytics() {
+export default function useAnalytics() {
   ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+  const { bgColor, titleColor } = useChartColor()
 
-  const ticketCountByPriority = await $fetch('/api/analytics/ticket-count-by-priority')
-  const bgColor = usePriorityColor()
-  const ticketCountByPriorityData = computed(() => {
-    return {
+  const ticketCountByPriorityData = ref({
+    labels: ['Baja', 'Media', 'Alta', 'Urgente'],
+    datasets: [{
+      data: [] as number[],
+      backgroundColor: bgColor.value,
+    }]
+  } satisfies ChartData)
+  const ticketCountByPriorityOptions = ref({
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        color: titleColor.value,
+        text: 'Total Tickets Abiertos',
+        font: {
+          size: 20,
+          weight: 'bold',
+        }
+      },
+    }
+  } satisfies ChartOptions)
+
+  const fetchData = async () => {
+    const ticketCountByPriority = await $fetch('/api/analytics/ticket-count-by-priority')
+    if (ticketCountByPriority.error) {
+      useToast({
+        title: 'Error al obtener los datos',
+        status: 'error',
+        type: 1,
+        autoclose: true,
+        autotimeout: 5000,
+      })
+    }
+
+    ticketCountByPriorityData.value = {
       labels: ['Baja', 'Media', 'Alta', 'Urgente'],
       datasets: [{
         data: Object.values(ticketCountByPriority.data) as number[],
         backgroundColor: bgColor.value,
       }]
     } satisfies ChartData
-  })
+  }
 
-  const ticketCountByPriorityOptions = computed(() => {
-    return {
-      responsive: true,
-      plugins: {
-        legend: {
-          display: false
-        },
-        title: {
-          display: true,
-          text: 'Conteo de tickets por prioridad',
-          font: {
-            size: 20,
-            weight: 'bold',
-          }
-        }
-      }
-    } satisfies ChartOptions
-  })
+  fetchData()
 
   return {
     ticketCountByPriorityData,
